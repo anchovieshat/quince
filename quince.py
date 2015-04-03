0739f8e16c54ed45b8ed72f0b89ce2e1d5306b44
Colin Davidson <colrdavidson@gmail.com>
2015-04-03 16:35:52 -0400

    Player Color! Also, enemies.

--- quince.py
+++ quince.py
@@ -1,19 +1,23 @@
 import pygame, sys
 from pygame import Rect
 
-tilesize = 32
-sprite_height = 32
-sprite_width = 32
+tilesize = 128
+sprite_height = tilesize
+sprite_width = tilesize
 screen_width = 1024
 screen_height = 1024
 tiles_per_row = screen_width / tilesize
 tiles_per_col = screen_height / tilesize
+left = 0
+front = 1
+back = 2
+right = 3
 
 class Game:
     def __init__(self, world_width, world_height):
         self.world = World(self, world_width, world_height)
-        self.entity_map = load_sprite("player.png", sprite_width, sprite_height)
-        self.player = Player(self, self.entity_map[1][0], 0, 0)
+        self.entity_map = load_sprite("assets/entitiesx128.png", sprite_width, sprite_height)
+        self.player = Player(self, self.entity_map[0], 0, 0)
         self.center_world()
 
     def center_world(self):
@@ -44,7 +48,7 @@ class GameObject:
 class World(GameObject):
     def __init__(self, game, width, height):
         GameObject.__init__(self, game)
-        self.tile_map = load_sprite("dirt32.png", sprite_width, sprite_height)
+        self.tile_map = load_sprite("assets/tilesx128.png", sprite_width, sprite_height)
         self.width = width
         self.height = height
         self.game_map = []
@@ -85,12 +89,13 @@ class Tile(GameObject):
             entity.draw(surface, x, y-(sprite_height/2))
 
 class Entity(GameObject):
-    def __init__(self, game, sprite, x, y):
+    def __init__(self, game, sprites, x, y):
         GameObject.__init__(self, game)
         self.game.world.game_map[x][y].add_entity(self)
-        self.sprite = sprite
+        self.sprites = sprites
         self.x = x
         self.y = y
+        self.anim_state = 0
 
     def move(self, x, y):
         self.move_to(self.x + x, self.y + y)
@@ -103,10 +108,45 @@ class Entity(GameObject):
         self.y = y
 
     def draw(self, surface, x, y):
-        surface.blit(self.sprite, (x, y))
+        surface.blit(self.sprites[self.anim_state], (x, y))
+
+    def turn(self, direction):
+        if len(self.sprites) < direction:
+            return
+        self.anim_state = direction
 
 class Player(Entity):
+    skin_color = pygame.Color(203, 203, 203)
+    bag_color = pygame.Color(152, 152, 152)
+    shirt_color = pygame.Color(136, 136, 136)
+    hat_rim_color = pygame.Color(123, 123, 123)
+    hat_color = pygame.Color(94, 94, 94)
+    boots_color = pygame.Color(84, 84, 84)
+
+    def __init__(self, game, sprites, x, y):
+        Entity.__init__(self, game, [], x, y)
+        self.original = sprites
+        for sprite in sprites:
+            sprite = sprite.copy()
+            px = pygame.PixelArray(sprite)
+            px.replace(self.skin_color, pygame.Color(255, 200, 190)) #Skin
+            px.replace(self.bag_color, pygame.Color(114, 49, 17)) #Bag
+            px.replace(self.shirt_color, pygame.Color(19, 67, 155)) #Body
+            px.replace(self.hat_rim_color, pygame.Color(27, 72, 155)) #Hat Rim
+            px.replace(self.hat_color, pygame.Color(32, 85, 181)) #Hat
+            px.replace(self.boots_color, pygame.Color(103, 61, 42)) #Boots
+            self.sprites.append(sprite)
+
     def move_to(self, x, y):
+        if self.x > x:
+            self.turn(left)
+        elif self.x < x:
+            self.turn(right)
+        if self.y > y:
+            self.turn(back)
+        elif self.y < y:
+            self.turn(front)
+
         if x >= self.game.world.width:
             return
         if x < 0:
@@ -122,10 +162,10 @@ def load_sprite(filename, width, height):
     image = pygame.image.load(filename).convert_alpha()
     image_width, image_height = image.get_size()
     sprite_table = []
-    for sprite_x in range(0, int(image_width/width)):
+    for sprite_y in range(0, int(image_height/height)):
         line = []
         sprite_table.append(line)
-        for sprite_y in range(0, int(image_height/height)):
+        for sprite_x in range(0, int(image_width/width)):
             rect = (sprite_x*width, sprite_y*height, width, height)
             line.append(image.subsurface(rect))
     return sprite_table
@@ -136,6 +176,7 @@ if __name__=='__main__':
     clock = pygame.time.Clock()
 
     game = Game(100, 100)
+    pygame.key.set_repeat(500, 30)
 
     while 1:
         for event in pygame.event.get():
@@ -150,6 +191,14 @@ if __name__=='__main__':
                     game.player.move(-1, 0)
                 if event.key == pygame.K_RIGHT:
                     game.player.move(1, 0)
+                if event.key == pygame.K_w:
+                    game.player.turn(back)
+                if event.key == pygame.K_s:
+                    game.player.turn(front)
+                if event.key == pygame.K_a:
+                    game.player.turn(left)
+                if event.key == pygame.K_d:
+                    game.player.turn(right)
                 if event.key == pygame.K_q:
                     sys.exit()
 
