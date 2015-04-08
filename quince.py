41d0dcc1c45035828b4bb875f1423c1509f09d4b
Colin Davidson <colrdavidson@gmail.com>
2015-04-08 18:50:47 -0400

    Menus!

--- quince.py
+++ quince.py
@@ -1,4 +1,6 @@
 import pygame, sys
+import pygame.freetype
+import pygame.sysfont
 from pygame import Rect
 
 tilesize = 128
@@ -8,53 +10,135 @@ screen_width = 1024
 screen_height = 1024
 tiles_per_row = screen_width / tilesize
 tiles_per_col = screen_height / tilesize
+world_width = 100
+world_height = 100
+fontsize = 15
 left = 0
 front = 1
 back = 2
 right = 3
 
 class Game:
-    def __init__(self, world_width, world_height):
-        self.world = World(self, world_width, world_height)
-        self.entity_map = load_sprite("assets/entitiesx128.png", sprite_width, sprite_height)
-        self.player = Player(self, self.entity_map[0], 0, 0)
-        self.monsters = []
-        self.monsters.append(Monster(self, self.entity_map[1], 1, 1))
-        self.center_world()
+    def __init__(self):
+        self.screen = MainMenu(self)
+        self.fonter = FontManager("cousine", fontsize, bold=True)
 
-    def center_world(self):
-        x = self.player.x
-        y = self.player.y
+    def switch_screen(self, screen):
+        self.screen = screen
 
-        x = x - (tiles_per_row//2)
-        y = y - (tiles_per_col//2)
+    def key_pressed(self, key_char):
+        self.screen.key_pressed(key_char)
 
-        if x+tiles_per_row > self.world.width:
-            x = self.view.left
-        if x < 0:
-            x = 0
-        if y+tiles_per_col-1 > self.world.height:
-            y = self.view.top
-        if y < 0:
-            y = 0
-
-        self.view = Rect((x,y),(x+tiles_per_row,y+tiles_per_col-1))
+    def update():
+        pass
 
     def draw(self, surface):
-        self.world.draw(self.view, surface)
+        self.screen.draw(surface)
 
-class GameObject:
+class WorldObject:
+    def __init__(self, world):
+        self.world = world
+
+class FontManager:
+    def __init__(self, name, size, **kwargs):
+        self.font = self.get_font(name, size, **kwargs)
+
+    def get_font(self, name, size, **kwargs):
+        return pygame.freetype.SysFont(name, size, **kwargs)
+
+    def draw(self, surface, pos, text, color):
+        self.font.render_to(surface, pos, text, color)
+
+class Screen:
     def __init__(self, game):
         self.game = game
 
-class World(GameObject):
+    def key_pressed(self, key_char):
+        pass
+
+    def draw(self, surface):
+        pass
+
+    def update(self, delta):
+        pass
+
+class Menu(Screen):
+    def __init__(self, game, items):
+        Screen.__init__(self, game)
+        self.items = items
+        self.selected = 0
+
+    def key_pressed(self, key_char):
+        i = 0
+        if key_char == pygame.K_w or key_char == pygame.K_UP:
+            i -= 1
+        if key_char == pygame.K_s or key_char == pygame.K_DOWN:
+            i += 1
+
+        self.selected += i
+
+        if self.selected < 0:
+            self.selected = len(self.items)-1
+        if self.selected >= len(self.items):
+            self.selected = 0
+
+        if key_char == pygame.K_RETURN:
+            self.items[self.selected].action()
+
+    def draw(self, surface):
+        for (i, item) in enumerate(self.items):
+            color = pygame.Color(255, 255, 255)
+            if i == self.selected:
+                color = pygame.Color(255, 0, 100)
+            rect = self.game.fonter.font.get_rect(item.text, size = fontsize)
+            self.game.fonter.draw(surface, ((screen_width/2) - ((rect.right - rect.left)/2), (screen_height/2)+(((rect.bottom - rect.top)+5)*i)), item.text, color)
+
+class MainMenu(Menu):
+    def __init__(self, game):
+        items = []
+        items.append(MenuItem("NEW GAME", lambda: self.game.switch_screen(World(self.game, world_height, world_width))))
+        items.append(MenuItem("CREDITS", lambda: self.game.switch_screen(CreditsMenu(self.game))))
+        items.append(MenuItem("EXIT", lambda: sys.exit()))
+        Menu.__init__(self, game, items)
+
+    def key_pressed(self, key_char):
+        if key_char == pygame.K_q:
+            sys.exit()
+
+        Menu.key_pressed(self, key_char)
+
+class CreditsMenu(Menu):
+    def __init__(self, game):
+        items = []
+        items.append(MenuItem("Crafted by Anchovieshat", lambda: None))
+        Menu.__init__(self, game, items)
+        self.selected = -1
+
+    def key_pressed(self, key_char):
+        if key_char == pygame.K_q:
+            self.game.switch_screen(MainMenu(self.game))
+
+        Menu.key_pressed(self, key_char)
+        self.selected = -1
+
+class MenuItem():
+    def __init__(self, text, action):
+        self.text = text
+        self.action = action
+
+class World(Screen):
     def __init__(self, game, width, height):
-        GameObject.__init__(self, game)
+        Screen.__init__(self, game)
+
+        self.entity_map = load_sprite("assets/entitiesx128.png", sprite_width, sprite_height)
         self.tile_map = load_sprite("assets/tilesx128.png", sprite_width, sprite_height)
+
+        self.game_map = []
         self.width = width
         self.height = height
-        self.game_map = []
+
         temp_surf = pygame.Surface((tilesize, tilesize))
+
         for x in range(0, width):
             line = []
             self.game_map.append(line)
@@ -62,19 +146,53 @@ class World(GameObject):
                 if y % 2 == 0 and x % 2 == 0:
                     surf = temp_surf.copy()
                     surf.fill(((y*10) % 255, (x*10)%255, (x*2)%255, 255))
-                    line.append(Tile(game, surf))
+                    line.append(Tile(self, surf))
                 else:
-                    line.append(Tile(game, self.tile_map[0][0]))
+                    line.append(Tile(self, self.tile_map[0][0]))
         self.monsters = []
+        self.monsters.append(Monster(self, self.entity_map[1], 1, 1))
 
-    def draw(self, rect, surface):
-        for x in range(rect.left, rect.width):
-            for y in range(rect.top, rect.height):
-                self.game_map[x][y].draw(surface, (x-rect.left)*tilesize, (y-rect.top+1)*tilesize)
+        self.player = Player(self, self.entity_map[0], 0, 0)
+        self.center()
 
-class Tile(GameObject):
-    def __init__(self, game, sprite):
-        GameObject.__init__(self, game)
+    def center(self):
+        x = self.player.x
+        y = self.player.y
+
+        x = x - (tiles_per_row//2)
+        y = y - (tiles_per_col//2)
+
+        if x+tiles_per_row > self.width:
+            x = self.view.left
+        if x < 0:
+            x = 0
+        if y+tiles_per_col-1 > self.height:
+            y = self.view.top
+        if y < 0:
+            y = 0
+
+        self.view = Rect((x,y),(x+tiles_per_row,y+tiles_per_col-1))
+
+    def key_pressed(self, key_char):
+        if key_char == pygame.K_w:
+            self.player.move(0, -1)
+        if key_char == pygame.K_s:
+            self.player.move(0, 1)
+        if key_char == pygame.K_a:
+            self.player.move(-1, 0)
+        if key_char == pygame.K_d:
+            self.player.move(1, 0)
+        if key_char == pygame.K_q:
+            self.game.switch_screen(MainMenu(self.game))
+
+    def draw(self, surface):
+        for x in range(self.view.left, self.view.width):
+            for y in range(self.view.top, self.view.height):
+                self.game_map[x][y].draw(surface, (x-self.view.left)*tilesize, (y-self.view.top+1)*tilesize)
+
+class Tile(WorldObject):
+    def __init__(self, world, sprite):
+        WorldObject.__init__(self, world)
         self.sprite = sprite
         self.entities = []
 
@@ -100,10 +218,10 @@ class Tile(GameObject):
         for entity in self.entities:
             entity.draw(surface, x, y-(sprite_height/2))
 
-class Entity(GameObject):
-    def __init__(self, game, sprites, x, y):
-        GameObject.__init__(self, game)
-        self.game.world.game_map[x][y].add_entity(self)
+class Entity(WorldObject):
+    def __init__(self, world, sprites, x, y):
+        WorldObject.__init__(self, world)
+        world.game_map[x][y].add_entity(self)
         self.sprites = sprites
         self.x = x
         self.y = y
@@ -113,7 +231,7 @@ class Entity(GameObject):
         self.move_to(self.x + x, self.y + y)
 
     def remove(self):
-        self.game.world.game_map[self.x][self.y].remove_entity(self)
+        self.world.game_map[self.x][self.y].remove_entity(self)
 
     def is_collidable(self):
         return True
@@ -122,8 +240,8 @@ class Entity(GameObject):
         pass
 
     def move_to(self, x, y):
-        game.world.game_map[self.x][self.y].remove_entity(self)
-        game.world.game_map[x][y].add_entity(self)
+        self.world.game_map[self.x][self.y].remove_entity(self)
+        self.world.game_map[x][y].add_entity(self)
 
         self.x = x
         self.y = y
@@ -144,8 +262,8 @@ class Player(Entity):
     hat_color = pygame.Color(94, 94, 94)
     boots_color = pygame.Color(84, 84, 84)
 
-    def __init__(self, game, sprites, x, y):
-        Entity.__init__(self, game, [], x, y)
+    def __init__(self, world, sprites, x, y):
+        Entity.__init__(self, world, [], x, y)
         self.original = sprites
         for sprite in sprites:
             sprite = sprite.copy()
@@ -168,21 +286,21 @@ class Player(Entity):
         elif self.y < y:
             self.turn(front)
 
-        if x >= self.game.world.width:
+        if x >= self.world.width:
             return
         if x < 0:
             return
-        if y >= self.game.world.height:
+        if y >= self.world.height:
             return
         if y < 0:
             return
 
-        if not self.game.world.game_map[x][y].is_collidable():
+        if not self.world.game_map[x][y].is_collidable():
             Entity.move_to(self, x, y)
-            self.game.center_world()
+            self.world.center()
 
         else:
-            self.game.world.game_map[x][y].collide(self)
+            self.world.game_map[x][y].collide(self)
 
 class Monster(Entity):
     body_color = pygame.Color(136, 136, 136)
@@ -236,7 +354,7 @@ if __name__=='__main__':
     screen = pygame.display.set_mode((screen_width, screen_height), pygame.DOUBLEBUF, 32)
     clock = pygame.time.Clock()
 
-    game = Game(100, 100)
+    game = Game()
     pygame.key.set_repeat(500, 30)
 
     while 1:
@@ -244,24 +362,7 @@ if __name__=='__main__':
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    game.player.move(0, 1)
-                if event.key == pygame.K_UP:
-                    game.player.move(0, -1)
-                if event.key == pygame.K_LEFT:
-                    game.player.move(-1, 0)
-                if event.key == pygame.K_RIGHT:
-                    game.player.move(1, 0)
-                if event.key == pygame.K_w:
-                    game.player.turn(back)
-                if event.key == pygame.K_s:
-                    game.player.turn(front)
-                if event.key == pygame.K_a:
-                    game.player.turn(left)
-                if event.key == pygame.K_d:
-                    game.player.turn(right)
-                if event.key == pygame.K_q:
-                    sys.exit()
+                game.key_pressed(event.key)
 
         screen.fill((0,0,0,0))
 
